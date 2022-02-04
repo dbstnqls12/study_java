e9d2c0ea640244aef43d1582ce2108b38ff30d55
dbstnqls12 <binson1123@naver.com>
2022-02-04 15:41:41 +0900

    asd

--- src/com/urdomain/lecture/ch02/Variable.java
+++ src/com/urdomain/lecture/ch02/Variable.java
@@ -34,7 +34,9 @@ public class Variable {
 		
 		c = a + b;
 		//변수 초기화해야 메모리에 생성되고 연산에 이용될 수 있다.
+		
 //		---------------------------------------------	
+		
 		//그러니 변수선언시 초기화하는 습관을 들이자.
 		
 		//정수형은 0
@@ -55,16 +57,35 @@ public class Variable {
 		
 		String hh = "";
 		
-		
+//		---------------------------------------------	
 		
 		//변수사용 (변수값 저장 & 변수값 읽음)
 		
 		//리터럴 = 데이터
+		// ex) 정수형리터럴 = 정수형 데이터
+		
 		//이스케이프 문자
+		System.out.println(c);
+		System.out.println("Subin Youn");
+		System.out.println("Subin Youn : " + c +" warm!");
+
+		System.out.println("한칸들여쓰기");
+		System.out.println("\t한칸들여쓰기");
+		System.out.println("\t\t한칸들여쓰기");
 		
+		System.out.println("한줄바꿈1");
+		System.out.println("\n한줄바꿈");
 		
+		System.out.println("한줄바꿈2");
+		System.out.println("\r한줄바꿈");
+
+		System.out.println("\'조금 졸리넴\'");
+		System.out.println("수빈 왈 \"아주 조금 졸려.\"");
+		System.out.println("\\");
 		
-	//	boolean defaultNy = 1;
+
+
+		
 		
 		
 
