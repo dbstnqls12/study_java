c3c45ae03a80b90f25844bd76a65511b3eeb4f16
dbstnqls12 <binson1123@naver.com>
2022-02-04 14:37:10 +0900

    asd

---
+++ src/com/urdomain/lecture/ch02/Variable.java
@@ -0,0 +1,32 @@
+package com.urdomain.lecture.ch02;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		
+		//변수 명명밥
+		//첫글자 소문자 그리고 다음 문자열부터는 대문자
+		//모드 변수명은 붙여서 사용
+		String fullName = "Subin Youn";
+		String name = "Subin Youn";
+		
+		
+		//대소문자 구분함
+		int roomNumber = 1;
+		int roomnumber = 2;
+		
+		System.out.println("roomnumber : "+roomnumber);
+		System.out.println("roomNumber : "+roomNumber);
+		
+		//예약어는 사용할 수 없다.
+		//int int = 2;
+		
+		
+		
+	//	boolean defaultNy = 1;
+		
+		
+
+	}
+
+}
