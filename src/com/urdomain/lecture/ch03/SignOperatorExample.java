fbbcb4f5f982e4ca9084123cf8a148f0694ab9a4
dbstnqls12 <binson1123@naver.com>
2022-02-09 15:57:33 +0900

    asd

--- src/com/urdomain/lecture/ch03/SignOperatorExample.java
+++ src/com/urdomain/lecture/ch03/SignOperatorExample.java
@@ -3,6 +3,24 @@ package com.urdomain.lecture.ch03;
 public class SignOperatorExample {
 	
 	public static void main(String[] args) {
+//			
+//		+ : 부호 그대로 유지
+//		- : 부호 변셩
+		
+		int x = -100;
+		
+		int result1 = +x; //
+
+		int result2 = -x; //
+
+		System.out.println("result1 = "+ result1);
+		System.out.println("result2 = "+ result2);
+	
+		short s = 100;
+//		short result3 = -s; //컴파일 익셉션
+		
+		int result3 = -s;
+		System.out.println("result3 = " + result3);
 		
 	}
 }
