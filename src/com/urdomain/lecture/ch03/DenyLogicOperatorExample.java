a76b08a7fe7ab838f5810adc91e08c1b5db7e53b
dbstnqls12 <binson1123@naver.com>
2022-02-09 17:12:46 +0900

    asd

--- src/com/urdomain/lecture/ch03/DenyLogicOperatorExample.java
+++ src/com/urdomain/lecture/ch03/DenyLogicOperatorExample.java
@@ -10,14 +10,13 @@ public class DenyLogicOperatorExample {
 //		boolean useNy = true;  	//1
 //		boolean delNy = false; 	//0
 		
-		System.out.println(play);
+		System.out.println(play); //true
 	
 		play = !play; // ! (논리부정연산자) : true면 false, false면 true
-		System.out.println(play);
+		System.out.println(play); //false
 		
 		play = !play; 
-		System.out.println(play);
-		
+		System.out.println(play); //true
 		
 	
 	}
