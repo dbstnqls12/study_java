3328c966c116fc96dd77b3e1fd1a8ebe4912caed
dbstnqls12 <binson1123@naver.com>
2022-02-10 16:49:18 +0900

    asd

--- src/com/urdomain/lecture/ch04/ConditionLoop.java
+++ src/com/urdomain/lecture/ch04/ConditionLoop.java
@@ -4,7 +4,61 @@ public class ConditionLoop {
 
 	public static void main(String[] args) {
 		
+//		조건문
+//			if
+//				if만 사용하는 경우 :  
+//				if(조건식) {
+//						
+//				}
 		
+//				조건이 true/false로 떨어지는 경우
+//				if(조건식) {
+//					
+//				}else { //else에는 조건식을 넣지 않는다
+//					
+//				}
+//				조건이 많은 경우
+//				if(조건식) {
+//					
+//				} else if (조건식) {
+//					
+//				} else if (조건식) {
+//					
+//				} else {
+//					
+//				}
+				
+//				번외
+//				if 안에 또 if를 넣을 수 있을까? 있음. 몇개까지? 무한대
+//				if() {
+//					if() {
+//						if() {
+//							
+//						}
+//					}
+//				}
 		
+//			switch
+//			
+//		반복문
+//			for
+//			
+//			while : 무한루프
+//				
+//			do-while
+//				
+//		참고
+//			break
+//			
+//			continue
+	
+		
+//		  for(int i=2; i<=9; i++) {
+//			  System.out.println(i+"의 구구단"); 
+//			  	for(int j=1; j<=9; j++) 
+//			  	{ System.out.println(+i+"X"+j+"="+(i*j));
+//			  	}
+//		  }
+		 
 	}
 }
