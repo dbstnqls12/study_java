bb27545d7d1458d3fbd1df48b325580e6be5f86f
dbstnqls12 <binson1123@naver.com>
2022-02-11 15:08:18 +0900

    asd

--- src/com/urdomain/lecture/ch04/IfNestExample.java
+++ src/com/urdomain/lecture/ch04/IfNestExample.java
@@ -4,7 +4,7 @@ public class IfNestExample {
 
 	public static void main(String[] args) {
 		
-		int score = 65;
+		int score = 98;
 		String grade = "";
 		
 		if(score >= 90) {
@@ -35,5 +35,58 @@ public class IfNestExample {
 			grade="F";
 		}
 		System.out.println("학점 : "+grade);
+	
+		System.out.println("--------------------------------");
+		
+		int x = 2;
+		int y = -2;
+		
+		if (x>0) {
+			if(y>0) {
+				System.out.println("제1사분면");
+				
+			}else if(y<0) {
+				System.out.println("제4사분면");
+				
+			}else {
+				System.out.println("범위 외");
+				
+			}
+		}else if(x<0){
+			if(y>0) {
+				System.out.println("제2사분면");
+			
+			}else if(y<0){
+				System.out.println("제3사분면");
+			
+			}else {
+				System.out.println("범위 외");
+			}
+		}else {
+			System.out.println("범위 외");
+		}
+			
+		System.out.println("--------------------");
+			
+		if(x>0 && y>0) {
+			System.out.println("제1사분면");
+		
+		}else if(x<0 && y>0) {
+			System.out.println("제2사분면");
+		
+		}else if(x<0 && y<0) {
+		System.out.println("제3사분면");
+		
+		}else if(x>0 && y<0) {
+		System.out.println("제4사분면");
+		
+		}else {
+			System.out.println("범위 외");
+		}
+
+		
+		
 	}
+	
+	
 }
