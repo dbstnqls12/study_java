247fc896240e6a3a4f05fb07de7c24dc63f205db
dbstnqls12 <binson1123@naver.com>
2022-02-15 16:33:53 +0900

    asd

--- src/com/urdomain/lecture/ch05/ArrayCreateByNewExample.java
+++ src/com/urdomain/lecture/ch05/ArrayCreateByNewExample.java
@@ -19,37 +19,45 @@ public class ArrayCreateByNewExample {
 		for(int i=0; i<3; i++) {
 			System.out.println("arr1[" + i + "] : " + arr1[i]);
 		}
-		
-		System.out.println("-------------------------------------");
-		
-		double[] arr2 = new double[3];
-		for(int i=0; i<3; i++) {
-			System.out.println("arr1[" + i + "] : " + arr2[i]);
-		}
+	
+		arr1[0] = 11;
+		arr1[1] = 22;
+		arr1[2] = 33;
 
-		arr2[0] = 1.0;
-		arr2[1] = 2.0;
-		arr2[2] = 3.0;
-		
 		for(int i=0; i<3; i++) {
-			System.out.println("arr1[" + i + "] : " + arr2[i]);
+			System.out.println("arr1[" + i + "] : " + arr1[i]);
 		}
 		
 		System.out.println("-------------------------------------");
 		
-		String[] arr3 = new String[3];
-		
-		for(int i=0; i<3; i++) {
-			System.out.println("arr1[" + i + "] : " + arr3[i]);
-		}
-
-		arr3[0] = "윤수빈";
-		arr3[1] = "송중기";
-		arr3[2] = "전지현";
-		
-		for(int i=0; i<3; i++) {
-			System.out.println("arr1[" + i + "] : " + arr3[i]);
-		}
+//		double[] arr2 = new double[3];
+//		for(int i=0; i<3; i++) {
+//			System.out.println("arr1[" + i + "] : " + arr2[i]);
+//		}
+//
+//		arr2[0] = 1.0;
+//		arr2[1] = 2.0;
+//		arr2[2] = 3.0;
+//		
+//		for(int i=0; i<3; i++) {
+//			System.out.println("arr1[" + i + "] : " + arr2[i]);
+//		}
+//		
+//		System.out.println("-------------------------------------");
+//		
+//		String[] arr3 = new String[3];
+//		
+//		for(int i=0; i<3; i++) {
+//			System.out.println("arr1[" + i + "] : " + arr3[i]);
+//		}
+//
+//		arr3[0] = "윤수빈";
+//		arr3[1] = "송중기";
+//		arr3[2] = "전지현";
+//		
+//		for(int i=0; i<3; i++) {
+//			System.out.println("arr1[" + i + "] : " + arr3[i]);
+//		}
 		
 		
 		
---
+++ src/com/urdomain/lecture/ch05/Exc.java
@@ -0,0 +1,19 @@
+package com.urdomain.lecture.ch05;
+
+public class Exc {
+
+	public static void main(String[] args) {
+		
+//		2부터 9까지 정수형 배열 변수만들고 for문을 사용하여 구구단 출력
+		
+		int[] gugu1 = {2,3,4,5,6,7,8,9};
+		int[] gugu2 = {1,2,3,4,5,6,7,8,9};
+	
+		for(int i=0; i<gugu1.length; i++) {
+			System.out.println("---- "+gugu1[i]+"단 ----");
+			for(int j=0; j<gugu2.length; j++) {
+				System.out.println(gugu1[i]+" X "+gugu2[j]+" = "+(gugu1[i]*gugu2[j]));
+			}
+		}
+	}
+}
