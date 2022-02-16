373ef865c4311786407ead9938d48ebc5c252683
dbstnqls12 <binson1123@naver.com>
2022-02-16 15:52:04 +0900

    sdf

---
+++ src/com/urdomain/lecture/ch05/Week.java
@@ -0,0 +1,16 @@
+package com.urdomain.lecture.ch05;
+
+public enum Week { //Enumberation Type
+	
+//	Enumberation constants : 열거 상수
+//	무조건 대문자
+//	복합문자는 언더바 사용 예) BLUE_MONDAY	
+	MONDAY,
+	TUESDAY,
+	WEDNESDAY,
+	THURSDAY,
+	FRIDAY,
+	SATURDAY,
+	SUNDAY
+	
+}
