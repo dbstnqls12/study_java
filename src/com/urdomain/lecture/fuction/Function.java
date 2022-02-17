c3c2f960289d2c62104b42596c39e86d21aa898a
dbstnqls12 <binson1123@naver.com>
2022-02-17 14:27:10 +0900

    asd

--- src/com/urdomain/lecture/fuction/Function.java
+++ src/com/urdomain/lecture/fuction/Function.java
@@ -8,23 +8,26 @@ public class Function {
 //		int sum = add(3, 4);
 //		System.out.println(sum);
 
-		add(3, 4);
-		System.out.println(add(3,4));
-		
-		int minus = minus(5,4);
-		System.out.println(minus);
-		
-		int multiply = multiply(1,4);
-		System.out.println(multiply);
+//		add(3, 4);
+//		System.out.println(add(3,4));
+//	
+//		
+//		int minus = minus(5,4);
+//		System.out.println(minus);
+//		
+//		int multiply = multiply(1,4);
+//		System.out.println(multiply);
+//
+//		int division= division(10,2);
+//		System.out.println(division);
+//		
 
-		int division= division(10,2);
-		System.out.println(division);
+
+		for(int a : allInOne(6,3)) {
+			System.out.println(a);
+		}
 		
-		int remainder = remainder(6,4);
-		System.out.println(remainder);
-
-
-				
+		System.out.println(str("윤수빈 ", "천재"));
 	}
 	
 //	public 리턴자료형 메소드명 (입력자료형1 입력변수1, 입력자료형2 입력변수2, ...) {
@@ -33,30 +36,59 @@ public class Function {
 //	}
 	
 //	함수 선언
+//	public static int add(int a, int b) {
+//		
+//		int result1 = a + b;
+//		return result1;
+//	}
+//	
+//	public static int minus(int a, int b) {
+//		return a-b;
+//	}
+//	
+//	public static int multiply(int a, int b) {
+//		int result3 = a * b;
+//		return result3;
+//	}
+//	public static int division(int a, int b) {
+//		
+//		int result4 = a / b;
+//		return result4;
+//	}
+//	public static int remainder(int a, int b) {
+//		
+//		int result5 = a % b;
+//		return result5;
+//	} 	
+	
+	public static int[] allInOne(int a, int b) {
+		
+		int sum = 0;
+		int sum2[] = new int[5];
+		sum2[0]= a+b;
+		sum2[1]= a-b;
+		sum2[2]= a*b;
+		sum2[3]= a/b;
+		sum2[4]= a%b;
+		
+		return sum2;
+ 	}
+	
+//	덧셈함수 
+//	input : int, int
+//	output : int 
 	public static int add(int a, int b) {
-		
-		int result1 = a + b;
-		return result1;
+		return 1;
 	}
 	
-	public static int minus(int a, int b) {
-		return a-b;
-	}
-	
-	public static int multiply(int a, int b) {
-		int result3 = a * b;
-		return result3;
-	}
-	public static int division(int a, int b) {
+	public static String str(String a, String b) {
 		
-		int result4 = a / b;
-		return result4;
-	}
-	public static int remainder(int a, int b) {
+		String str1=a+b;
+		return str1;
 		
-		int result5 = a % b;
-		return result5;
 	} 	
+	
+	
 	
 
 	
