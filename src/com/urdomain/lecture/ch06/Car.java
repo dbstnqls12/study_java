a224f45a7c87ac2673c1b07009aef4f81a696c15
dbstnqls12 <binson1123@naver.com>
2022-02-17 17:14:44 +0900

    asd

---
+++ src/com/urdomain/lecture/ch06/Car.java
@@ -0,0 +1,16 @@
+package com.urdomain.lecture.ch06;
+
+public class Car {
+
+//	field
+	String Company = "현대자동차";
+	String Model = "그렌다이저";
+	String color= "white";
+
+	int maxSpeed = 240;
+	int Speed = 0;
+	
+//	method
+	
+	
+}
---
+++ src/com/urdomain/lecture/ch06/Member.java
@@ -0,0 +1,15 @@
+package com.urdomain.lecture.ch06;
+
+public class Member {
+
+	String name = "윤수빈";
+	String id = "qwe123";
+	String pwd = "1111";
+	String address= "서울시 무슨구 어디동";
+	int mobile = 01012311231;
+	int gender = 1;
+	int nationality = 1;
+	String hobby= "운동";
+	String color= "검정";
+	
+}
