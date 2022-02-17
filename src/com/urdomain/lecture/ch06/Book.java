79d73c1b7ff30e80774fe92e267714181e93f2db
dbstnqls12 <binson1123@naver.com>
2022-02-17 17:41:23 +0900

    asd

---
+++ src/com/urdomain/lecture/ch06/Book.java
@@ -0,0 +1,13 @@
+package com.urdomain.lecture.ch06;
+
+import java.util.Date;
+
+public class Book {
+
+	String title = "해커스토익";
+	String subTitle = "Reading Rc";
+	String author = "David cho";
+	String publisher = "해커스 어학연구소";
+	Date publicationDate;
+
+}
--- src/com/urdomain/lecture/ch06/Member.java
+++ src/com/urdomain/lecture/ch06/Member.java
@@ -1,14 +1,16 @@
 package com.urdomain.lecture.ch06;
 
+import java.util.Date;
+
 public class Member {
 
 	String name = "윤수빈";
 	String id = "qwe123";
 	String pwd = "1111";
 	String address= "서울시 무슨구 어디동";
+	Date Dob;
 	int mobile = 01012311231;
 	int gender = 1;
-	int nationality = 1;
 	String hobby= "운동";
 	String color= "검정";
 	
