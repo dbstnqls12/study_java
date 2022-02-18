f4d68715ed6e13bfb70d69462cca8fff87c5019d
dbstnqls12 <binson1123@naver.com>
2022-02-18 15:48:55 +0900

    asd

--- src/com/urdomain/lecture/ch06/Member.java
+++ src/com/urdomain/lecture/ch06/Member.java
@@ -14,4 +14,40 @@ public class Member {
 	String hobby= "운동";
 	String color= "검정";
 	
+//	method
+	
+//	등록, 저장
+	public void save() {
+		
+	}
+	
+//	삭제
+	public void delete() {
+		
+	}
+	
+//	수정
+	
+//	읽기
+	
+//	찾기
+	
+//	동의
+	
+//	아이디중복체크
+	
+//	중복확인
+	
+//	주소검색
+	
+//	비밀번호 관련
+	
+//	로그인
+	
+//	로그아웃
+	
+	
+	
+	
+	
 }
