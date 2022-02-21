6c3d5b49af2fd28c186e2ac819daee29a183e475
dbstnqls12 <binson1123@naver.com>
2022-02-21 15:53:28 +0900

    asd

---
+++ src/com/urdomain/lecture/ch06/FieldIntValue.java
@@ -0,0 +1,19 @@
+package com.urdomain.lecture.ch06;
+
+public class FieldIntValue {
+	
+	//field
+	byte byteField;
+	short shortField;
+	int intField;
+	long longField;
+	
+	boolean booleanField;
+	char charField;
+	
+	float floatField;
+	double doubleField;
+	
+	int[] arrfield;
+	String referenceField;
+}
---
+++ src/com/urdomain/lecture/ch06/NewCar.java
@@ -0,0 +1,13 @@
+package com.urdomain.lecture.ch06;
+
+public class NewCar {
+
+	public NewCar() {
+		
+	}
+	
+	//overroad : 오버로드 : 다형성
+	public NewCar(String color, int cc) {
+		
+	}
+}
