537c8c01f078aa42d419a8fc14c33ea35fdf0e88
dbstnqls12 <binson1123@naver.com>
2022-03-08 17:32:19 +0900

    asd

--- src/com/urdomain/lecture/ch08/Example.java
+++ src/com/urdomain/lecture/ch08/Example.java
@@ -7,7 +7,7 @@ public class Example {
 		InterfaceB interfaceB = new ImplementationC();
 		InterfaceC interfaceC = new ImplementationC();
 		
-		interfaceA.methodA();
+		interfaceA.methodA();	//본인 인터페이스에 있는것만 사용가능
 		interfaceB.methodB();
 		interfaceC.methodC();
 		
