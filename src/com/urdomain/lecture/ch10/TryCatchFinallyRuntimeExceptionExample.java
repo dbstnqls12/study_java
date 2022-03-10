5c823499ea048e94581a0ba020f760ee0a3bf2c0
dbstnqls12 <binson1123@naver.com>
2022-03-10 17:06:32 +0900

    asd

--- src/com/urdomain/lecture/ch10/TryCatchFinallyRuntimeExceptionExample.java
+++ src/com/urdomain/lecture/ch10/TryCatchFinallyRuntimeExceptionExample.java
@@ -4,22 +4,30 @@ public class TryCatchFinallyRuntimeExceptionExample {
 
 	public static void main(String[] args) {
 	
-//		사전 조치가 더 중요함
+//		사전 처리가 중요함
 		
 		String data1 = null;
 		String data2 = null;
 		
-		System.out.println(args[0]);
-		System.out.println(args[1]);
+		try {
+			data1 = args[0];
+			data1 = args[1];
+		}catch(ArrayIndexOutOfBoundsException e) {	//예외는 경험적으로 작성을 하는 부분이 있고 모호하면 Exception사용
+			System.out.println("실행 매개값의 수가 부족합니다.");
+			System.out.println("[실행방법]");
+			System.out.println("java TryCatchFinallyRuntimeExceptionExample num1 num2");
+			return;
+		}
+		try {
+			int value1 = Integer.parseInt(data1);	
+			int value2 = Integer.parseInt(data2);
+			int result = value1 + value2;
+			System.out.println(data1 + "+" + data2 + "=" + result);
+		}catch(NumberFormatException e) {			//예외는 경험적으로 작성을 하는 부분이 있고 모호하면 Exception사용
+			System.out.println("숫자로 변환할 수 없습니다.");
+		}finally{
+			System.out.println("다시 실행하세요");
+		}
 		
-		data1 = args[0];
-		data1 = args[1];
-
-		int value1 = Integer.parseInt(data1);
-		int value2 = Integer.parseInt(data2);
-		int result = value1 + value2;
-		
-		System.out.println(data1 + "+" + data2 +"="+result);
-
 	}
 }
